c1e5152c116207a0f849e4b06c3d81bf6781942f
Volcomix <volcomix@gmail.com>
2019-07-03 22:49:38 +0200

    Enhance envelope demo with effects

--- src/music/instruments/demo/Envelope.js
+++ src/music/instruments/demo/Envelope.js
@@ -1,6 +1,12 @@
 import Instrument from '../../common/Instrument'
 
 export default class Envelope extends Instrument {
+  peakGain = 1
+  sustainGain = 0.5
+  attackDuration = 0.2
+  decayDuration = 0.2
+  releaseDuration = 0.5
+
   start() {
     this.oscillator = this.audioContext.createOscillator()
     this.oscillator.frequency.value = 172.3
@@ -18,12 +24,38 @@ export default class Envelope extends Instrument {
   noteOn(noteFrequency, time) {
     this.oscillator.frequency.setValueAtTime(noteFrequency, time)
     this.gain.gain.setValueAtTime(0, time)
-    this.gain.gain.linearRampToValueAtTime(1, time + 0.2)
-    this.gain.gain.linearRampToValueAtTime(0.5, time + 0.4)
+    this.gain.gain.linearRampToValueAtTime(
+      this.peakGain,
+      time + this.attackDuration,
+    )
+    this.gain.gain.linearRampToValueAtTime(
+      this.sustainGain,
+      time + this.attackDuration + this.decayDuration,
+    )
   }
 
   noteOff(time) {
-    this.gain.gain.setValueAtTime(0.5, time)
-    this.gain.gain.linearRampToValueAtTime(0, time + 0.5)
+    this.gain.gain.setValueAtTime(this.sustainGain, time)
+    this.gain.gain.linearRampToValueAtTime(0, time + this.releaseDuration)
+  }
+
+  fxPeakGain(gain) {
+    this.peakGain = gain / 255
+  }
+
+  fxSustainGain(gain) {
+    this.sustainGain = gain / 255
+  }
+
+  fxAttackDuration(duration) {
+    this.attackDuration = duration / 255
+  }
+
+  fxDecayDuration(duration) {
+    this.decayDuration = duration / 255
+  }
+
+  fxReleaseDuration(duration) {
+    this.releaseDuration = duration / 255
   }
 }
