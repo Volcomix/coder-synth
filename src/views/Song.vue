31de02f91abd7d52d1f874720d6203b94aab18dd
Volcomix <Volcomix@users.noreply.github.com>
2019-09-21 17:22:46 +0200

    Fix audio worklet loading

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -110,7 +110,7 @@ export default {
       await Promise.all(
         Object.values(audioWorklets).map(async audioWorklet => {
           await this.audioContext.audioWorklet.addModule(
-            `/audio-worklets/${audioWorklet}.js`,
+            `audio-worklets/${audioWorklet}.js`,
           )
         }),
       )
